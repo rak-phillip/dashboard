fa4df9090de84aa1d0fcd9bd67dbcb09baee7777
Phillip Rak <phillip@opensuse.philrakisback.com>
2021-10-06 13:36:41 -0700

    Add checkbox to list of exported components

--- components/main.js
+++ components/main.js
@@ -1,10 +1,13 @@
 import BadgeState from './BadgeState.vue'
 import Card from './Card.vue'
+import Checkbox from './form/Checkbox.vue'
 
 export { BadgeState }
 export { Card }
+export { Checkbox }
 
 export default {
   BadgeState,
-  Card
+  Card,
+  Checkbox
 }
