5239c421caaed2fb9a5661c0650dca2b2d65f814
Phillip Rak <rak.phillip@gmail.com>
2022-08-15 10:13:39 -0700

    Replace sort
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- pkg/rancher-utils/src/index.ts
+++ pkg/rancher-utils/src/index.ts
@@ -1,5 +1,6 @@
-export * from './string';
+export * from './string/string';
 export * from './array';
 export * from './object';
 export * from './platform';
 export * from './position';
+export * from './sort/sort';
--- pkg/rancher-utils/src/sort/sort.js
+++ pkg/rancher-utils/src/sort/sort.js
@@ -1,5 +1,5 @@
-import { get } from './object';
-import { strPad } from './string';
+import { get } from '../object';
+import { strPad } from '../string';
 
 // Based on https://github.com/emberjs/ember.js/blob/master/packages/@ember/-internals/runtime/lib/type-of.js
 // and      https://github.com/emberjs/ember.js/blob/master/packages/@ember/-internals/runtime/lib/mixins/array.js
@@ -43,50 +43,50 @@ const TYPE_MAP = {
 const { toString } = Object.prototype;
 
 /**
-  Returns a consistent type for the passed object.
+ Returns a consistent type for the passed object.
 
-  Use this instead of the built-in `typeof` to get the type of an item.
-  It will return the same result across all browsers and includes a bit
-  more detail. Here is what will be returned:
+ Use this instead of the built-in `typeof` to get the type of an item.
+ It will return the same result across all browsers and includes a bit
+ more detail. Here is what will be returned:
 
-      | Return Value  | Meaning                                              |
-      |---------------|------------------------------------------------------|
-      | 'string'      | String primitive or String object.                   |
-      | 'number'      | Number primitive or Number object.                   |
-      | 'boolean'     | Boolean primitive or Boolean object.                 |
-      | 'null'        | Null value                                           |
-      | 'undefined'   | Undefined value                                      |
-      | 'function'    | A function                                           |
-      | 'array'       | An instance of Array                                 |
-      | 'regexp'      | An instance of RegExp                                |
-      | 'date'        | An instance of Date                                  |
-      | 'filelist'    | An instance of FileList                              |
-      | 'error'       | An instance of the Error object                      |
-      | 'object'      | A JavaScript object                                  |
+ | Return Value  | Meaning                                              |
+ |---------------|------------------------------------------------------|
+ | 'string'      | String primitive or String object.                   |
+ | 'number'      | Number primitive or Number object.                   |
+ | 'boolean'     | Boolean primitive or Boolean object.                 |
+ | 'null'        | Null value                                           |
+ | 'undefined'   | Undefined value                                      |
+ | 'function'    | A function                                           |
+ | 'array'       | An instance of Array                                 |
+ | 'regexp'      | An instance of RegExp                                |
+ | 'date'        | An instance of Date                                  |
+ | 'filelist'    | An instance of FileList                              |
+ | 'error'       | An instance of the Error object                      |
+ | 'object'      | A JavaScript object                                  |
 
-  Examples:
+ Examples:
 
-  import { typeOf } from '@shell/utils/type-of';
+ import { typeOf } from '@shell/utils/type-of';
 
-  typeOf();                       // 'undefined'
-  typeOf(null);                   // 'null'
-  typeOf(undefined);              // 'undefined'
-  typeOf('michael');              // 'string'
-  typeOf(new String('michael'));  // 'string'
-  typeOf(101);                    // 'number'
-  typeOf(new Number(101));        // 'number'
-  typeOf(true);                   // 'boolean'
-  typeOf(new Boolean(true));      // 'boolean'
-  typeOf(A);                      // 'function'
-  typeOf([1, 2, 90]);             // 'array'
-  typeOf(/abc/);                  // 'regexp'
-  typeOf(new Date());             // 'date'
-  typeOf(event.target.files);     // 'filelist'
-  typeOf(new Error('teamocil'));  // 'error'
+ typeOf();                       // 'undefined'
+ typeOf(null);                   // 'null'
+ typeOf(undefined);              // 'undefined'
+ typeOf('michael');              // 'string'
+ typeOf(new String('michael'));  // 'string'
+ typeOf(101);                    // 'number'
+ typeOf(new Number(101));        // 'number'
+ typeOf(true);                   // 'boolean'
+ typeOf(new Boolean(true));      // 'boolean'
+ typeOf(A);                      // 'function'
+ typeOf([1, 2, 90]);             // 'array'
+ typeOf(/abc/);                  // 'regexp'
+ typeOf(new Date());             // 'date'
+ typeOf(event.target.files);     // 'filelist'
+ typeOf(new Error('teamocil'));  // 'error'
 
-  // 'normal' JavaScript object
-  typeOf({ a: 'b' });             // 'object'
-*/
+ // 'normal' JavaScript object
+ typeOf({ a: 'b' });             // 'object'
+ */
 export function typeOf(item) {
   if (item === null) {
     return 'null';
