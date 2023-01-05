e6c580191dfb8951b9868e7e5af36dd8d8032b9d
Phillip Rak <rak.phillip@gmail.com>
2023-01-05 09:48:47 -0700

    Replace menu svg with icon
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- shell/components/ActivityBar.vue
+++ shell/components/ActivityBar.vue
@@ -42,18 +42,7 @@ export default Vue.extend({
         class="menu-container"
         @click="menuClick"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          height="24"
-          viewBox="0 0 24 24"
-          width="24"
-        >
-          <path
-            d="M0 0h24v24H0z"
-            fill="none"
-          />
-          <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
-        </svg>
+        <span class="icon icon-2x icon-menu" />
       </div>
     </div>
     <div class="body">
