bf8c12bc6b387eaeb8e783c7cfc3acf19de08c2a
Phillip Rak <rak.phillip@gmail.com>
2023-01-05 09:33:33 -0700

    Update layout to include activity bar
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- shell/layouts/desktop.vue
+++ shell/layouts/desktop.vue
@@ -1,6 +1,68 @@
+<script>
+import ActivityBar from '@shell/components/ActivityBar';
+import Brand from '@shell/mixins/brand';
+import BrowserTabVisibility from '@shell/mixins/browser-tab-visibility';
+import { mapState } from 'vuex';
+
+export default {
+  name: 'LayoutDesktop',
+
+  components: { ActivityBar },
+
+  mixins: [Brand, BrowserTabVisibility],
+
+  middleware: ['authenticated'],
+
+  computed: { ...mapState(['managementReady']) },
+};
+</script>
+
 <template>
-  <div>
-    DESKTOP - NOT FAIL
-    <Nuxt />
+  <div class="dashboard-root">
+    <div class="dashboard-content">
+      <activity-bar class="activity-bar" />
+      <div>Mock Header</div>
+      <main class="main-layout">
+        <nuxt class="outlet" />
+      </main>
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+  .dashboard-root {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
+
+  .dashboard-content {
+    display: grid;
+    flex-grow:1;
+
+    grid-template-areas:
+      "activity-bar header"
+      "activity-bar main";
+
+    grid-template-columns: max(57px) auto;
+    grid-template-rows:    var(--header-height) auto;
+
+    > HEADER {
+      grid-area: header;
+    }
+  }
+
+  MAIN {
+    grid-area: main;
+    overflow: auto;
+
+    .outlet {
+      min-height: 100%;
+      padding: 0;
+    }
+  }
+
+  .activity-bar {
+    grid-area: activity-bar;
+  }
+</style>
