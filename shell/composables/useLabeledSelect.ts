6fe77ad9db72e5a85487910a22ffcc3ee72c734e
Phillip Rak <rak.phillip@gmail.com>
2026-04-23 22:02:19 +0000

    Migrate LabeledSelect away from LabeledFormElement mixin
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

---
+++ shell/composables/useLabeledSelect.ts
@@ -0,0 +1,61 @@
+import { computed, ComputedRef, Ref, nextTick } from 'vue';
+import { getWidth, setWidth } from '@shell/utils/width';
+
+interface LabeledSelectProps {
+  options?: Array<any>;
+  searchable?: boolean;
+  filterable?: boolean;
+  canPaginate?: boolean;
+}
+
+interface UseLabeledSelect {
+  isSearchable: ComputedRef<boolean>;
+  isFilterable: ComputedRef<boolean>;
+  resizeHandler: (selectRef: Ref<HTMLElement | null>) => void;
+}
+
+export const useLabeledSelect = (props: LabeledSelectProps): UseLabeledSelect => {
+  const isSearchable = computed(() => {
+    if (props.canPaginate) {
+      return true;
+    }
+    const options = props.options || [];
+
+    if (props.searchable || options.length >= 10) {
+      return true;
+    }
+
+    return false;
+  });
+
+  const isFilterable = computed(() => {
+    if (props.canPaginate) {
+      return false;
+    }
+
+    return props.filterable ?? true;
+  });
+
+  const resizeHandler = (selectRef: Ref<HTMLElement | null>) => {
+    // since the DD is positioned there is no way to 'inherit' the size of the input, this calcs the size of the parent and set the dd width if it is smaller. If not let it grow with the regular styles
+    nextTick(() => {
+      if (!selectRef.value) {
+        return;
+      }
+
+      const DD = selectRef.value.querySelector('ul.vs__dropdown-menu');
+      const selectWidth = getWidth(selectRef.value) || 0;
+      const dropWidth = getWidth(DD as Element) || 0;
+
+      if (dropWidth < selectWidth) {
+        setWidth(DD as Element, selectWidth);
+      }
+    });
+  };
+
+  return {
+    isSearchable,
+    isFilterable,
+    resizeHandler
+  };
+};
